a053744935ff67715ea12bc4d1ede8939b1d82dc
Ratalx <k.szczygiel166@gmail.com>
2019-03-12 01:33:25 +0100

    1st step of making Polygons

--- source/main.cpp
+++ source/main.cpp
@@ -4,52 +4,72 @@
 #include<GLFW/glfw3.h>
 #include<iostream>
 #include<string>
+#include<vector>
 
-float vertices[] = 
-{
-    0.5f,  0.5f,  0.0f,
-    0.5f,   -0.5f,  0.0f,
 
-    -0.5f,  -0.5f,  0.0f,
-    -0.5f,  0.5f,   0.0f
-};
-unsigned int indices[] =
-{
-    0,1,3, //first triangle
-    1,2,3 //second triangle
-};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void InitializeWindow();
+GLFWwindow* InitializeWindow();
 
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);   
-
-
-    auto* window = glfwCreateWindow(800,600,"Learn OpenGL",nullptr,nullptr);
-    if (window == nullptr)
+    float sizeOfRow =2;
+    float vertices[] = 
     {
-        std::cerr<<"Failed to Create GLFW Window"<<std::endl;
-        glfwTerminate();
-        return -1;
+        0.5f,  0.5f,  0.0f,
+        0.5f,   -0.5f,  0.0f,
+
+        -0.5f,  -0.5f,  0.0f,
+        -0.5f,  0.5f,   0.0f
+    };
+    std::vector<float> vertices2;
+    for(float i =0;i<=sizeOfRow;++i)
+    {
+        for(float j =0;j<=sizeOfRow;++j)
+        {
+            vertices2.emplace_back( -1.f+j*(2.0f/sizeOfRow) );
+            vertices2.emplace_back( 1.f -i*(2.0f/sizeOfRow));
+            vertices2.emplace_back(0);
+        }
+    }
+    std::vector<unsigned int> indices2;
+    //indices2.resize(sizeOfRow*sizeOfRow*3);
+    
+        for(int i=0;i<sizeOfRow*sizeOfRow;i=+sizeOfRow)
+        {
+
+                indices2.emplace_back(i);
+                indices2.emplace_back(i+1);
+                indices2.emplace_back(i+sizeOfRow+2);
+                indices2.emplace_back(i);
+                indices2.emplace_back(i+sizeOfRow+1);
+                indices2.emplace_back(i+sizeOfRow+2);
+            
+        }
+    
+    int k =0;
+    for(auto i : indices2)
+    {
+k++;
+        std::cout << i <<" ";
+        if(k==3)
+        {
+            k=0;
+        std::cout<<"\n";
+        }
     }
 
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    unsigned int indices[] =
     {
-        std::cerr << "Failed to initialize GLAD"<<std::endl;
-        return -1;
-    }
-    glViewport(0,0,800,600);
+        0,1,3, //first triangle
+       1,2,3 //second triangle
+    };
 
+    auto* window = InitializeWindow();
+    
     Shader ourShader("vertexShader.vs",
                      "fragmentShader.fs");
     
@@ -60,11 +80,11 @@ int main()
 
     glGenBuffers(1,&VBO);
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices2.size()*sizeof(float),&vertices2[0],GL_STATIC_DRAW);
     
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,  indices2.size()*sizeof( unsigned int), &indices2[0], GL_STATIC_DRAW);
 
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -78,14 +98,13 @@ int main()
 
 
 
-        glClearColor(226/255.0f,208/255.0f,208/255.0f,1.0f);
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -110,9 +129,33 @@ void processInput(GLFWwindow* window)
     }
 }
 
-void InitializeWindow()
+GLFWwindow* InitializeWindow()
 {
-    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);   
+
+
+    auto* window = glfwCreateWindow(800,600,"Learn OpenGL",nullptr,nullptr);
+    if (window == nullptr)
+    {
+        std::cerr<<"Failed to Create GLFW Window"<<std::endl;
+        glfwTerminate();
+        throw std::runtime_error("GLFW Load Failed");
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD"<<std::endl;
+        throw std::runtime_error("Glad Load Failed");
+    }
+    glViewport(0,0,800,600);
+
+    return window;
 }
 void InitializeOpenGL();
 void InitializeBuffers();
